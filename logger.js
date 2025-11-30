f73d21e17a5027a301bc40d19b509b366cdbe9d4
ahmadjvd <ahmadjvd23@gmail.com>
2025-11-30 22:59:44 +0400

    added timestamp to logger

--- logger.js
+++ logger.js
@@ -2,10 +2,13 @@ function logger(req, res, next) {
     console.log("Logger Middleware Triggered!"); // Debugging line
     
     // Log the request method and URL
-    console.log("---------------------------");
-    console.log("New request received:");
-    console.log("Method:", req.method);
-    console.log("URL:", req.url);
+        console.log("---------------------------");
+        console.log("New request received:");
+        console.log("Timestamp:", timestamp);
+        console.log("Method:", req.method);
+        console.log("URL:", req.url);
+        console.log("Status Code:", res.statusCode);
+        console.log("---------------------------");
 
 
     
